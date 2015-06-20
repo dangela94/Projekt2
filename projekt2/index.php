7f01cce307e1ab0b2ec0ff78e7be039e4b7f536b
Tomasz Dudek <tomasz.dudek@firma.interia.pl>
2015-06-20 18:54:54 +0200

    WYlaczam https

--- projekt2/index.php
+++ projekt2/index.php
@@ -1,9 +1,9 @@
 <?php
-if($_SERVER["HTTPS"] != "on") {
+/*if($_SERVER["HTTPS"] != "on") {
 header("HTTP/1.1 301 Moved Permanently");
 header("Location: https://". $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]);
 exit();
-}
+}*/
 ?>
 <?php
 
